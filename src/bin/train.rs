8f661214f81bc4c0abea4d5f26d2dfdd57e79427
Thomas Svendal <thomassvendal@gmail.com>
2025-08-12 13:11:22 +0200

    testing other flake

--- src/bin/train.rs
+++ src/bin/train.rs
@@ -1,6 +1,7 @@
+use burn_cuda::CudaDevice;
 use minesweeper::ai::{train, MyAutodiffBackend};
 
 fn main() {
-    let device = dbg!(Default::default());
+    let device = dbg!(CudaDevice::default());
     train::<MyAutodiffBackend>(device);
 }
